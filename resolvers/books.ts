4637c31c000ed1de69558ab86fda94d37fd8977a
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-08-26 11:49:55 -0500

    feat: basic setup with ts

---
+++ resolvers/books.ts
@@ -0,0 +1,10 @@
+// Resolvers define how to fetch the types defined in your schema.
+import { reviews, games, authors } from "../mocks/videogames.js";
+
+export const videoGamesResolvers = {
+  Query: {
+    reviews: () => reviews,
+    games: () => games,
+    authors: () => authors,
+  },
+};
---
+++ schemas/videogames.ts
@@ -0,0 +1,26 @@
+export const videoGamesDefs = `#graphql
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each.
+  type Query {
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+  }
+`;
